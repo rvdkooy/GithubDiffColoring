96f3db2034d1e9eb34350be72944970d76d308ff
Ronald van der Kooij <ronald@sharpsolutions.nl>
2014-12-14 20:13:20 +0100

    not using jquery anymore but rendering an inline style tag

--- js/default.js
+++ js/default.js
@@ -1,25 +1,24 @@
-$(function(){
+chrome.storage.sync.get(GitHubDiffSettings.settingsKey, function(result) {
 
-    
+    if(result.diffsettings){
+        result = JSON.parse(result.diffsettings);
+    }
+    else{
+        result.additionGutter = GitHubDiffSettings.defaults.additionGutter;
+        result.additionCode = GitHubDiffSettings.defaults.additionCode;
+        result.deletionGutter = GitHubDiffSettings.defaults.deletionGutter;
+        result.deletionCode = GitHubDiffSettings.defaults.deletionCode;
+    }
 
-    chrome.storage.sync.get(GitHubDiffSettings.settingsKey, function(result) {
+    var style = document.createElement('style');
+	style.type = 'text/css';
+	
+	var innerHtml = '.blob-num-addition { background-color: #' + result.additionGutter + ' !important;  }';
+	innerHtml += '.blob-code-addition { background-color: #' + result.additionCode + ' !important;  }';
+	innerHtml += '.blob-num-deletion { background-color: #' + result.deletionGutter + ' !important;  }';
+	innerHtml += '.blob-code-deletion { background-color: #' + result.deletionCode + ' !important;  }';
 
-        if(result.diffsettings){
-            result = JSON.parse(result.diffsettings);
-        }
-        else{
-            result.additionGutter = GitHubDiffSettings.defaults.additionGutter;
-            result.additionCode = GitHubDiffSettings.defaults.additionCode;
-            result.deletionGutter = GitHubDiffSettings.defaults.deletionGutter;
-            result.deletionCode = GitHubDiffSettings.defaults.deletionCode;
-        }
-
-        $('.blob-num-addition').css('background-color', '#' + result.additionGutter);
-        $('.blob-num-addition').css('color', 'white');
-        $('.blob-code-addition').css('background-color', '#' + result.additionCode);
-
-        $('.blob-num-deletion').css('background-color', '#' + result.deletionGutter);
-        $('.blob-num-deletion').css('color', 'white');
-        $('.blob-code-deletion').css('background-color', '#' + result.deletionCode);
-    });
-})
+	style.innerHTML = innerHtml
+	
+	document.getElementsByTagName('head')[0].appendChild(style);
+});
